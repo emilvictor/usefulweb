f2dc99e410cafe71815e12ce1136cdddc045272a
Emil Pettersson <61893425+emilvictor@users.noreply.github.com>
2021-04-18 14:31:59 +0200

    small css change

--- src/components/SingleMeal.js
+++ src/components/SingleMeal.js
@@ -10,6 +10,7 @@ function SingleMeal(props) {
             <h3>{meal.strMeal}</h3> 
            <img src={meal.strMealThumb} alt="food"/>
             <div className="ingredients">
+            <h2>Ingredients</h2>
                {ingredients.map((ingredient, idx)=><p key={idx}>{meal[ingredient]}</p>)}
                 </div> 
         </div>
